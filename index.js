d0cc35cd50ab5aa8cccd610ebb11e2e4c1a8a9f2
Huzaifa-Syed1 <huzaifasd9@gmail.com>
2026-06-13 04:20:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let A1 = document.getElementById("p1")
+let count = 0
+function a1(){
+    count += 1
+    A1.textContent = count
+}
+function a2(){
+    count += 2
+    A1.textContent = count
+}
+function a3(){
+    count += 3
+    A1.textContent = count
+}
+
+let A2 = document.getElementById("p2")
+let count1 = 0
+function b1(){
+    count1 += 1
+    A2.textContent = count1
+}
+function b2(){
+    count1 += 2
+    A2.textContent = count1
+}
+function b3(){
+    count1 += 3
+    A2.textContent = count1
+}
+    
+
+
